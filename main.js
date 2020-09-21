d4741884565086be6636b245103e09fd2934e79b
Dominic Faulring <domcodesjs@gmail.com>
2020-09-21 13:31:51 -0700

    done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const main = document.querySelector('main');
-  const form = document.querySelector('form');
-  const inputValue = document.querySelector('input');
+  const imagesForm = document.querySelector('.dog-images-form');
+  const breedForm = document.querySelector('.dog-breed-form');
+  const imageInputValue = document.querySelector('.img-input');
+  const breedInputValue = document.querySelector('.breed-input');
 
   async function getDogs(count) {
     let images = '';
@@ -15,18 +17,48 @@ document.addEventListener('DOMContentLoaded', function () {
     return images;
   }
 
-  form.addEventListener('submit', handleFormSubmit);
+  async function getBreedImage(breed) {
+    try {
+      const res = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
 
-  async function handleFormSubmit(e) {
+      const data = await res.json();
+
+      if (data.status !== 'success') {
+        return 'Breed does not exist!';
+      }
+
+      const { message } = data;
+
+      return `<div><img src="${message[0]}" alt="Dog Img"></div>`;
+    } catch (err) {
+      console.log(err);
+      return 'Breed does not exist!';
+    }
+  }
+
+  imagesForm.addEventListener('submit', handleImagesFormSubmit);
+  breedForm.addEventListener('submit', handleBreedFormSubmit);
+
+  async function handleBreedFormSubmit(e) {
+    e.preventDefault();
+
+    if (breedInputValue.value.trim() === '') {
+      return;
+    }
+
+    return (main.innerHTML = await getBreedImage(breedInputValue.value));
+  }
+
+  async function handleImagesFormSubmit(e) {
     e.preventDefault();
 
     if (
-      inputValue.value.trim() === '' ||
-      inputValue.value < 1 ||
-      inputValue.value > 50
+      imageInputValue.value.trim() === '' ||
+      imageInputValue.value < 1 ||
+      imageInputValue.value > 50
     ) {
       return;
     }
-    return (main.innerHTML = await getDogs(inputValue.value));
+    return (main.innerHTML = await getDogs(imageInputValue.value));
   }
 });
